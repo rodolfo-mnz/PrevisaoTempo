4ba8e5f51ade57c2120e1a092dabd549b969e868
Rodolfo Menezes <dolfogameplay@gmail.com>
2024-07-08 04:15:46 -0300

    first commit

---
+++ src/scripts.js
@@ -0,0 +1,22 @@
+const key = "a7bda5652c5f500aae21cf9f0a3209a1"
+
+
+function colocarDadosNaTela(dados) {
+
+    console.log(dados);
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+    buscarCidade(cidade)
+}
